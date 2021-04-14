3d5dee0f5e359705ae74a00a8c9ea44ebcc6b224
argenaden <69447666+argenaden@users.noreply.github.com>
2021-04-15 00:17:10 +0900

    article/tags/familiar tags

--- src/pages/tag/index.js
+++ src/pages/tag/index.js
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from "antd";
-import { tagService } from 'services'
+import {articleService, tagService} from 'services'
 import { FormattedMessage } from 'react-intl'
-import { useParams } from 'react-router-dom'
-import ArticleView3 from "../../components/blrmn/ArticleView3";
+import {Link, useParams} from 'react-router-dom'
+import ArticleView3 from "components/blrmn/ArticleView3";
 
 const Tags = () => {
 
   const [articles, setArticles] = useState([]);
+  const [tags, setTags ] = useState([])
   const [page, setPage] = useState();
   const [loading, setLoading] = useState(false);
   const params = useParams();
 
   useEffect(() => {
     loadTags();
+    loadFamiliarTags();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const loadTags = (pageNumber, size) =>{
     setLoading(true)
-    tagService.getArticlesByTagName(params.tagName, pageNumber, size)
+    articleService.getArticlesByTagName(params.tagName, pageNumber, size)
       .then(result => {
         const p = result.data
         setPage(p)
@@ -28,16 +30,28 @@ const Tags = () => {
       })
   }
 
+  const loadFamiliarTags = () =>{
+    tagService.getFamiliarTags(params.tagName, 5)
+      .then(result=> {
+        setTags(result.data)
+      })
+  }
+
   return (
     <div>
-      {articles && articles.length > 0 && (
-        <div className="font-size-14">
-          <FormattedMessage id="pages.tag.index.tagged_in" />
-          <span className="font-size-24 font-weight-bold" style={{textTransform: "capitalize"}}>
-            {params.tagName}
-          </span>
-        </div>
-      )}
+      <div className="mb-3">
+        {tags.map(tag => (
+          <Link to={`/tags/${tag.name}`} className="badge text-blue text-capitalize bg-light font-size-16 mr-2 mb-2">
+            #{tag.name}
+          </Link>
+        ))}
+      </div>
+      <div className="font-size-14">
+        <FormattedMessage id="pages.tag.index.tagged_in" />
+        <span className="font-size-24 font-weight-bold" style={{textTransform: "capitalize"}}>
+          {params.tagName}
+        </span>
+      </div>
       {articles && articles.map(article => <ArticleView3 article={article} key={`article_${article.id}`} />)}
       {page && page.totalPages > 1 && page.number + 1 < page.totalPages && (
         <div className="mb-5 pb-2">
--- src/services/tag/index.js
+++ src/services/tag/index.js
@@ -1,16 +1,24 @@
 import apiClient from 'services/axios'
 
 // eslint-disable-next-line import/prefer-default-export
-export const  tagService = {
-  getArticlesByTagName
+export const tagService = {
+  getTags,
+  getTagByName,
+  getFamiliarTags
 };
 
-function getArticlesByTagName(tagName, page, size){
-  let url = `/articles/tags?`
-  if(tagName) url = `${url}tagName=${tagName}`;
-  if(page) url = `${url}&page=${page}`;
+function getTags(){
+  return apiClient.get(`/tags`);
+}
+
+function getTagByName(tagName){
+  return apiClient.get(`/tags/${tagName}`);
+}
+
+function getFamiliarTags(tagName, size){
+  let url = `/tags/familiar?`
+  if(tagName) url = `${url}&tagName=${tagName}`;
   if(size) url = `${url}&size=${size}`;
   return apiClient.get(url);
-
 }
 
