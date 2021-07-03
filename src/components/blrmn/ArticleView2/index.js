c8c86fb58672c35d743ccc623a619f94563254cb
asanoviskhak <asanov.personal@gmail.com>
2021-07-03 10:53:40 +0900

    :lipstick: Fixed user profile overflow on ArtcleView2 UI.

--- src/components/blrmn/ArticleView2/index.js
+++ src/components/blrmn/ArticleView2/index.js
@@ -1,20 +1,42 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-import Avatar from "components/blrmn/Avatar";
-import Clap from "components/blrmn/Clap";
-import Bookmark from "components/blrmn/Bookmark";
-import ViewCount from "components/blrmn/ViewCount";
-import CommentCount from "components/blrmn/CommentCount";
-import config from "config";
-import style from "./style.module.scss";
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from 'components/blrmn/Avatar'
+import Clap from 'components/blrmn/Clap'
+import Bookmark from 'components/blrmn/Bookmark'
+import ViewCount from 'components/blrmn/ViewCount'
+import CommentCount from 'components/blrmn/CommentCount'
+import config from 'config'
+import style from './style.module.scss'
 
-const ArticleView2 = ({article}) => {
+const ArticleView2 = ({ article }) => {
+  const [newText, setNewText] = useState({
+    title: article.title,
+    subtitle: article.subtitle,
+  })
+  useEffect(() => {
+    const len = article.title.split(' ').length
+    console.log(len)
+    if (len > 5 && len <= 8) {
+      setNewText({ title: article.title, subtitle: article.subtitle.substr(0, 40).concat('. .') })
+    } else if (len > 8 && len < 12) {
+      setNewText({ title: article.title, subtitle: '' })
+    } else if (len >= 12) {
+      setNewText({
+        title: article.title.substr(0, 75).concat('. .'),
+        subtitle: '',
+      })
+    }
+  }, [])
+
   return (
     <div className="card border-0">
       <div
         className={`${style.head}`}
         style={{
-          backgroundImage: article.imageId != null ? `url(${config.apiUrl}/images/article/${article.imageId})` : 'url(/resources/images/template/1.jpeg)',
+          backgroundImage:
+            article.imageId != null
+              ? `url(${config.apiUrl}/images/article/${article.imageId})`
+              : 'url(/resources/images/template/1.jpeg)',
         }}
       >
         <div className={`${style.overlay} d-flex flex-nowrap`}>
@@ -22,29 +44,48 @@ const ArticleView2 = ({article}) => {
             <div className="d-flex">
               <div className="mr-auto">
                 <Link to={`/article/details/${article.id}`}>
-                  <h4 className="text-white font-weight-bold mr-3">{article.title}</h4>
+                  <h4 className="text-white font-weight-bold mr-3 article-title-text">
+                    {newText.title}
+                  </h4>
                 </Link>
               </div>
-              <div><Bookmark articleId={article.id} type="white" /></div>
+              <div>
+                <Bookmark articleId={article.id} type="white" />
+              </div>
             </div>
             <div className="mt-2 mb-auto">
               <Link to={`/article/details/${article.id}`}>
-                <div className="text-white font-size-14" dangerouslySetInnerHTML={{__html: article.subtitle.substr(0,100).concat('. .')}} />
+                {newText.subtitle.length > 1 && (
+                  <div
+                    className="text-white font-size-14"
+                    dangerouslySetInnerHTML={{
+                      __html: newText.subtitle,
+                    }}
+                  />
+                )}
               </Link>
             </div>
             <div className="d-flex">
               <div className="mr-auto mt-auto">
                 <div className={style.item}>
-                  <div className="mr-2"><Avatar author={article.user} size={27} type="primary" /></div>
+                  <div className="mr-2">
+                    <Avatar author={article.user} size={27} type="primary" />
+                  </div>
                   <Link to={`/author/profile/${article.user.id}`}>
                     <div className="text-primary mr-2">{`${article.user.name} ${article.user.surname}`}</div>
                   </Link>
                 </div>
               </div>
               <div className="ml-auto d-flex">
-                <span className="mr-3"><ViewCount count={article.views} /></span>
-                <span className="mr-3"><CommentCount count={article.comments} /></span>
-                <span><Clap article={article} type="white" /></span>
+                <span className="mr-3">
+                  <ViewCount count={article.views} />
+                </span>
+                <span className="mr-3">
+                  <CommentCount count={article.comments} />
+                </span>
+                <span>
+                  <Clap article={article} type="white" />
+                </span>
               </div>
             </div>
           </div>
--- src/pages/article/details/comments/comments.js
+++ src/pages/article/details/comments/comments.js
@@ -1,31 +1,29 @@
-import React, {useState,
-  useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { FormattedMessage, injectIntl } from 'react-intl'
-import {Button, Form, Input} from "antd";
-import {commentService} from 'services';
-import Comment from "./comment";
+import { Button, Form, Input } from 'antd'
+import { commentService } from 'services'
+import Comment from './comment'
 
 const { TextArea } = Input
 
-const mapStateToProps = ({ user }) => ({user})
-const Comments = ({user, article, intl: { formatMessage }}) => {
+const mapStateToProps = ({ user }) => ({ user })
+const Comments = ({ user, article, intl: { formatMessage } }) => {
+  const [comments, setComments] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [loadingComments, setLoadingComments] = useState(false)
+  const [page, setPage] = useState()
 
-  const [comments, setComments] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [loadingComments, setLoadingComments] = useState(false);
-  const [page, setPage] = useState();
-
-  const [form] = Form.useForm();
+  const [form] = Form.useForm()
 
   useEffect(() => {
-    loadComments();
+    loadComments()
   }, [])
 
-  const loadComments = (pageNumber, size) =>{
+  const loadComments = (pageNumber, size) => {
     setLoadingComments(true)
     commentService.getCommentsByArticle(article.id, pageNumber, size).then(result => {
-      const {data} = result;
+      const { data } = result
       setComments([...comments, ...data.content])
       setPage(data)
       setLoadingComments(false)
@@ -36,28 +34,30 @@ const Comments = ({user, article, intl: { formatMessage }}) => {
     if (article.id) {
       setLoading(true)
       values.articleId = article.id
-      values.user = user;
+      values.user = user
       commentService.saveComment(values).then(result => {
         setComments([...comments, result.data])
         setLoading(false)
-        form.resetFields();
-      });
+        form.resetFields()
+      })
     }
   }
 
-  const updateComment = (comment) => {
+  const updateComment = comment => {
     return commentService.saveComment(comment).then(result => {
-      const {data} = result
-      const index = comments.findIndex(function f(c) {return c.id === data.id;});
-      comments[index] = {...comments[index], message: data.message, updatedAt: data.updatedAt}
-      setComments(comments);
-    });
+      const { data } = result
+      const index = comments.findIndex(function f(c) {
+        return c.id === data.id
+      })
+      comments[index] = { ...comments[index], message: data.message, updatedAt: data.updatedAt }
+      setComments(comments)
+    })
   }
 
-  const deleteComment = (id) => {
+  const deleteComment = id => {
     return commentService.deleteComment(id).then(() => {
       setComments([...comments.filter(x => x.id !== id)])
-    });
+    })
   }
 
   return (
@@ -69,27 +69,53 @@ const Comments = ({user, article, intl: { formatMessage }}) => {
         <Form className="login-form" onFinish={onFinish} form={form}>
           <Form.Item
             name="message"
-            rules={[{ required: true, message: formatMessage({ id: 'article.details.comments.commentMessageError'}) }]}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'article.details.comments.commentMessageError' }),
+              },
+            ]}
           >
-            <TextArea rows={3} placeholder={formatMessage({ id: 'article.details.comments.commentMessage'})} />
+            <TextArea
+              rows={3}
+              placeholder={formatMessage({ id: 'article.details.comments.commentMessage' })}
+            />
           </Form.Item>
           <Form.Item>
-            <Button className="float-right btn btn-default" loading={loading} htmlType="submit" style={{ width: 150 }}>
+            <Button
+              className="float-right btn btn-default"
+              loading={loading}
+              htmlType="submit"
+              style={{ width: 150 }}
+            >
               <i className="fe fe-message-square mr-2" />
               <FormattedMessage id="article.details.comments.comment" />
             </Button>
           </Form.Item>
         </Form>
         <h6 className="mb-4 text-uppercase">
-          <strong><FormattedMessage id="article.details.comments.comments" /> ({article.comments})</strong>
+          <strong>
+            <FormattedMessage id="article.details.comments.comments" /> ({article.comments})
+          </strong>
         </h6>
-        {comments.map(comment => (<Comment key={`comment_${comment.id}`} comment={comment} deleteComment={deleteComment} updateComment={updateComment} />))}
+        {comments.map(comment => (
+          <Comment
+            key={`comment_${comment.id}`}
+            comment={comment}
+            deleteComment={deleteComment}
+            updateComment={updateComment}
+          />
+        ))}
 
-        {page && page.totalPages > 1 && page.number + 1 < page.totalPages &&
-        <Button className="btn btn-default btn-block mt-3" onClick={() => loadComments(page.number + 1)} loading={loadingComments}>
-          <FormattedMessage id="article.details.comments.loadMore" />
-        </Button>
-        }
+        {page && page.totalPages > 1 && page.number + 1 < page.totalPages && (
+          <Button
+            className="btn btn-default btn-block mt-3"
+            onClick={() => loadComments(page.number + 1)}
+            loading={loadingComments}
+          >
+            <FormattedMessage id="article.details.comments.loadMore" />
+          </Button>
+        )}
       </div>
     </div>
   )
